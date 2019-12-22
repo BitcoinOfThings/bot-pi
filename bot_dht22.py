59122bbf95a0b087b10056d546ee673b5b04a909
Dave Foderick <dfoderick@gmail.com>
2019-12-22 10:19:57 -0800

    add humidity to published message

--- bot_dht22.py
+++ bot_dht22.py
@@ -1,6 +1,8 @@
 import Adafruit_DHT
 import paho.mqtt.client as mqtt
 import json
+import time
+import datetime
 
 mqttc = mqtt.Client()
 mqttc.username_pw_set(username="demo", password="demo")
@@ -8,14 +10,19 @@ mqttc.connect("mqtt.bitcoinofthings.com")
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
+topic = "demo/temp"
 
+print("publishing on {0}".format(topic))
 print ("Reading DHT22...")
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-        mqttc.publish("bot_demo", json.dumps({"clientId":"demo", "message":"{0:0.1f}".format(temperature)}))
+        dt = datetime.datetime.now().ctime()
+        msgtext = '{0}: Temp={1:0.1f}*C Humidity={2:0.1f}%'.format(dt, temperature, humidity)
+        print(msgtext)
+        mqttc.publish(topic, json.dumps({"clientId":"demo", "message":msgtext}))
+        time.sleep(60)
     else:
         print("Failed to retrieve data from humidity sensor")
 
