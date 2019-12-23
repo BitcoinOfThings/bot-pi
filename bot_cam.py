6746e74ad59eb4138714635d688e84fed1f3e52b
Dave Foderick <dfoderick@gmail.com>
2019-12-22 16:30:51 -0800

    added time caption to camera

--- bot_cam.py
+++ bot_cam.py
@@ -4,16 +4,20 @@ import paho.mqtt.client as mqtt
 import json
 import os.path
 import sys
+import datetime
 
 CAMFILE = "bot.jpg"
-CAMWAIT = 10
+CAMHEIGHT = 200 
+CAMWIDTH = 300
+CAMWAIT = 60
+topic = "demo/cam"
 
 def take_pic():
     camera = PiCamera()
-    camera_annotate_text_size = 60
-    camera.annotate_text = "Uploaded using BitcoinOfThings.com"
+    camera_annotate_text_size = 32
+    camera.annotate_text = "BitcoinOfThings.com {0}".format(datetime.datetime.now().ctime())
     camera.framerate = 15
-    camera.resolution = (800, 600)
+    camera.resolution = (CAMWIDTH, CAMHEIGHT)
     camera.image_effect = 'cartoon'
     camera.start_preview(alpha=200)
     #camera.start_recording('bot.h264')
@@ -27,15 +31,21 @@ mqttc = mqtt.Client()
 mqttc.username_pw_set(username="demo", password="demo")
 mqttc.connect("mqtt.bitcoinofthings.com")
 
+print('publishing pics to {0}'.format(topic))
+
 try:
     while True:
         take_pic()
         if os.path.isfile(CAMFILE):
             with open(CAMFILE, mode='rb') as file:
                 contents = file.read()
-                jMessage = {"clientId":"demo", "message": contents.hex()}
-                mqttc.publish("bot_demo", json.dumps(jMessage))
-                print("published picture")
+                # todo: if posting to blockchain need to fix the fee for large files
+                pic = contents.hex() #[0:25]
+                #print(pic)
+                jMessage = {"clientId":"demo", "message": pic}
+                #print(jMessage)
+                mqttc.publish(topic, json.dumps(jMessage))
+                print("{0}: published pic".format(datetime.datetime.now().ctime()))
             os.remove(CAMFILE)
         else:
             print("No picture taken")
